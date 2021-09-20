4fb88daf7c5085c5a4e159d864c85420e9bcc334
jmbr2004 <91036107+jmbr2004@users.noreply.github.com>
2021-09-20 09:56:16 -0500

    Update WappiOrdenConCupon.java
    Se hicieron cambios para la ejecución exitosa de compra con cupon

--- src/test/java/wappi/pageobject/cupon/WappiOrdenConCupon.java
+++ src/test/java/wappi/pageobject/cupon/WappiOrdenConCupon.java
@@ -6,56 +6,58 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 
 public class WappiOrdenConCupon extends PageObject {
-	
+
 	@FindBy(css = "#welcome-coupon")
 	WebElementFacade botonCupon;
-	
-	@FindBy(css = "modal-content")
-	WebElementFacade codigoCupon;
-	
+
+	@FindBy(xpath = "//input[@id='coupon']")
+	WebElementFacade ingCodigoCupon;
+
 	@FindBy(css = "div.home:nth-child(4) div.home-content table.app-table.offers-table tbody:nth-child(2) tr:nth-child(2) td.align-center:nth-child(7) > button.button.success.offers-table-block-button")
 	WebElementFacade botonPedir;
-	
+
 	@FindBy(css = "#order-confirm")
 	WebElementFacade botonConfirmar;
-	
+
 	@FindBy(css = "app-root:nth-child(1) app-coupon-modal:nth-child(2) div.modal.opened div.modal-content > span.close:nth-child(1)")
 	WebElementFacade botonCerrarPopup;
 
-	@FindBy(xpath = "//input[@id='coupon']")
+	@FindBy(css = "body:nth-child(2) app-root:nth-child(1) app-coupon-modal:nth-child(2) div.modal.opened > div.modal-content")
 	WebElementFacade textoCodigoCupon;
-	
+
 	@FindBy(linkText = "Inicio")
 	WebElementFacade botonInicio;
 	
-	
-	public void pedirCupon() {
-		
+	String codigoCupon;
+
+	public void irBotCup() {
+
 		botonCupon.click();
 	}
 
-	public void pedirCompra() {
-		
-		botonPedir.click();
-	}
+	public void obtenerCofigoCupon() {
 
-	public void confirmarCompra() {
-		
-		botonConfirmar.click();
+		codigoCupon = textoCodigoCupon.findElement(By.xpath("//*[@id=\"coupon-code\"]")).getAttribute("innerHTML");
 	}
 	
-	public void cerrarPopup() {
-		
+	public void irBotCerCup() {
+
 		botonCerrarPopup.click();
 	}
+	
 	public void irInicio() {
-		
+
 		botonInicio.click();
 	}
 
-	public void obtenerCofigoCupon() {
+	public void pedirCompra() {
+
+		botonPedir.click();
+	}
+
+	public void confirmarCompraCupon() {
 		
-		String codigo = textoCodigoCupon.findElement(By.xpath("//*[@id=\"coupon-code\"]")).getAttribute("innerHTML");
-		textoCodigoCupon.type(codigo);
+		ingCodigoCupon.type(codigoCupon);
+		botonConfirmar.click();
 	}
 }
